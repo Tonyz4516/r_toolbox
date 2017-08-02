8c00341e0e7ea6468436ef42db083b8d4e4abc12
wscrdzg <yukuang.zhang@gmail.com>
2017-08-02 15:11:45 -0700

    Add files via upload

--- googlesankey.R
+++ googlesankey.R
@@ -1,15 +1,15 @@
 # sample format: [ 'A', 'X', 5 ],
-print("This function is for prepare list for google chart sankey, function called googlesankey()")
-print(paste0("output to ", getwd(), "/a.txt"))
-print("sample format: [ 'A', 'X', 5 ],")
-print("address is https://developers.google.com/chart/interactive/docs/gallery/sankey")
-   
+cat("This function is for prepare list for google chart sankey, function called googlesankey()")
+cat(paste0("output to ", getwd(), "/a.txt"))
+cat("sample format: [ 'A', 'X', 5 ],")
+cat("address is https://developers.google.com/chart/interactive/docs/gallery/sankey")
+
 googlesankey <- function(df) {
   if (length(df) == 3) {
-    sink("a.txt")
+    #sink("a.txt")
     for (i in 1:length(df[,1])) {
-      print(paste0("[ '",df[i,1],"', '",df[i,2],"', ",df[i,3]," ],"))
+      cat(paste0("[ '",df[i,1],"', '",df[i,2],"', ",df[i,3]," ],\n"))
     }
-    sink()
+    #sink()
   } else {stop("Please choose data frame with 3 cols!")}
 }
