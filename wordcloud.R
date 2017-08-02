efe03f98cd18eee951f34cae7fb7c9d96ed558dd
wscrdzg <yukuang.zhang@gmail.com>
2017-08-02 15:23:25 -0700

    Update wordcloud.R

--- wordcloud.R
+++ wordcloud.R
@@ -1,11 +1,10 @@
 cat("Many thanks to https://www.jasondavies.com/wordcloud/")
 cat("This function convert data frame with Col1: word and Col2: frequency to single vector\ncall wordcloud()")
 wordcloud <- function(dataframe) {
-  if (length(dataframe) != 2) {stop("Please input df with 2 cols")}
-  x <- vector(mode = "character")
+  if (length(dataframe[1,]) != 2) {stop("Please input df with 2 cols")}
   for(i in 1:length(dataframe[,1])){
     for (m in 1:dataframe[i,2]){
-      x <- c(x,dataframe[i,1])
+      cat(paste0(dataframe[i,1],"\n"))
     }
   }
 }
