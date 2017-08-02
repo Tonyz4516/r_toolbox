aa7a4f6ce28cdda8e3e2040e416110977e4bcc56
wscrdzg <yukuang.zhang@gmail.com>
2017-08-02 15:42:45 -0700

    Update wordcloud.R

--- wordcloud.R
+++ wordcloud.R
@@ -1,14 +1,14 @@
-cat("Many thanks to https://www.jasondavies.com/wordcloud/\n")
-cat("And warning: this website automatically remove common words\nso if you want to output airport name like CAN, it will not be displayed\n")
+cat("Many thanks to https://www.jasondavies.com/wordcloud/\n \n")
+cat("And warning: this website automatically remove common words\nso if you want to output airport name like CAN, it will not be displayed\n \n")
 cat("This function convert data frame with Col1: word and Col2: frequency to single vector\ncall wordcloud()")
 wordcloud <- function(dataframe) {
-  count <- 0
+  #count <- 0
   if (length(dataframe[1,]) != 2) {stop("Please input df with 2 cols")}
   for(i in 1:length(dataframe[,1])){
     for (m in 1:dataframe[i,2]){
-      cat(paste0(dataframe[i,1],"\n"))
-      count <- count + 1
+      cat(paste0(dataframe[i,1]," "))
+      #count <- count + 1
     }
   }
-  print(count)
+  #print(count)
 }
