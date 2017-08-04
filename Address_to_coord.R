024df3c91e3741468071f89dfb703c44e18aceae
wscrdzg <yukuang.zhang@gmail.com>
2017-08-04 14:02:58 -0700

    Update Address_to_coord.R

--- Address_to_coord.R
+++ Address_to_coord.R
@@ -1,14 +1,25 @@
 addresstocoord <- function(address) {
-  require(RJSONIO)
-  url <- "http://maps.google.com/maps/api/geocode/json?address="
-  url <- URLencode(paste(url, address, "&sensor=false", sep = ""))
-  x <- fromJSON(url, simplify = FALSE)
-  if (x$status == "OK") {
-    out <- c(x$results[[1]]$geometry$location$lng,
-             x$results[[1]]$geometry$location$lat)
-  } else {
-    out <- NA
+  # check if address is a vector
+  if(!is.vector(address)) {
+    stop("address must be vector!")
   }
-  Sys.sleep(0.2)  # API only allows 5 requests per second
-  out
+  require(RJSONIO)
+  # build new dataframe with 2 variables
+  df <- data.frame(0,0)
+  df <- df[-1,]
+  colnames(df) <- c("latitude","longitude")
+  # loop address through google api
+  for (i in 1:length(address)) {
+    url <- URLencode(paste("http://maps.google.com/maps/api/geocode/json?address=", address[i], "&sensor=false", sep = ""))
+    x <- fromJSON(url, simplify = FALSE)
+    if (x$status == "OK") {
+      df[i,2] <- x$results[[1]]$geometry$location$lng
+      df[i,1] <- x$results[[1]]$geometry$location$lat
+    } else {
+      df[i,1] <- NA
+      df[i,2] <- NA
+    }
+    Sys.sleep(0.2)  # API only allows 5 requests per second
+  }
+  df
 }
